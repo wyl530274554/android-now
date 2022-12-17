458543ac03078caa87bb97c7ba556b93e8731f79
melon <530274554@qq.com>
2022-12-17 16:44:43 +0800

    添加systemProperties展示

--- app/src/main/java/com/melon/unity/function/gallery/GalleryViewModel.java
+++ app/src/main/java/com/melon/unity/function/gallery/GalleryViewModel.java
@@ -4,13 +4,25 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.Properties;
+
 public class GalleryViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
     public GalleryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+//        mText.setValue("This is gallery fragment");
+        Properties properties = System.getProperties();
+        StringBuilder sb = new StringBuilder();
+        sb.append("total size: "+properties.stringPropertyNames().size()+"\n\n");
+
+        for(String key:properties.stringPropertyNames()){
+            sb.append("key: "+ key+"\n");
+            String val = properties.getProperty(key);
+            sb.append("val: "+val+"\n\n");
+        }
+        mText.setValue(sb.toString());
     }
 
     public LiveData<String> getText() {
